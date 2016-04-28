e196cebf5efa47a38f47317c9c917cf7cb5871b5
Asher Israel <artificialisrael@yahoo.com>
2016-04-28 09:58:44 -0700

    working on rendering images

---
+++ public/assets/components/login_controller-012a2d7ba79420f71cd3eddd8b85f6c94561da7268717c70bfc9d48e063d5132.jsx
@@ -0,0 +1,14 @@
+class LoginController extends React.Component {
+	render () {
+		return(
+			<div className="container-fluid">
+				<br/><br/>
+				<pre>
+					<Register/>
+					<br/>
+					<Login/>
+				</pre>
+			</div>
+		);
+	}
+}
